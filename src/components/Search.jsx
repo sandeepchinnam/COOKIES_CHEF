1c85ce72b08ee6c7b3ff680080c093b8673f47d1
sandeepchinnam <94036941+sandeepchinnam@users.noreply.github.com>
2023-05-09 12:34:51 +0530

    Add files via upload

---
+++ src/components/Search.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+function Search() {
+    const [input, setInput] = useState('');
+    const navigate = useNavigate();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate(`/searched/${input}`);
+    };
+
+    return (
+        <FormStyle onSubmit={submitHandler}>
+            <div>
+                <FaSearch />
+                <input
+                    onChange={(e) => setInput(e.target.value)}
+                    type='text'
+                    value={input}
+                />
+            </div>
+        </FormStyle>
+    );
+}
+
+const FormStyle = styled.form`
+    margin: 0;
+    width: 100%;
+    div {
+        position: relative;
+        width: 100%;
+    }
+    input {
+        border: none;
+        background: rgb(0,255,255);
+        background: linear-gradient(90deg, rgba(0,255,255,1) 0%, rgba(0,255,255,1) 75%, rgba(89,115,115,1) 85%);
+        font-size: 1.5rem;
+        color: #00000;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 100%;
+    }
+    svg {
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        color: white;
+    }
+`;
+
+export default Search;
